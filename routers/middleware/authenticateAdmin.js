aebfe25dd566d6748ab9f9ef9fdf0160c6c1130b
Jason <jasonbohio@gmail.com>
2024-01-08 11:05:34 -0500

    update

--- routers/middleware/authenticateAdmin.js
+++ routers/middleware/authenticateAdmin.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const User = require("../../db/models/user");
 
 module.exports = (req, res, next) => {
  // Extracting the token from the request header
@@ -6,6 +7,8 @@ module.exports = (req, res, next) => {
 
  // Setting up the JWT secret for token verification
  const secret = process.env.JWT_SECRET || "Satoshi Nakamoto";
+ // Setting the admin key for admin verification
+ const key = process.env.ADMIN_KEY || "1234";
 
  // If token is present, attempt to verify it using the JWT module
  if (token) {
@@ -15,26 +18,22 @@ module.exports = (req, res, next) => {
        res.status(401).json({ message: "Error with your verification" });
      } else {
        // If token is verified, find the user using their username from the database
-       // Placeholder user object - later we will fetch the real user from the database
-       const user = {
-        username: "test",
-        password: "pass1",
-        adminKey: 1234,
-      };
-      // Extracting admin key from user object if it exists
-      const adminKey = user?.adminKey?.toString() ?? "";
-      // Checking if extracted admin key matches with the one in env variables
-      if (adminKey !== process.env.ADMIN_KEY) {
-        // If admin key does not match, return 401 error
-        res.status(401).json({ message: "Must be an admin" });
-      } else {
-        // If admin key matches, let the endpoint continue executing
-        next();
-      }
-    }
-  });
-} else {
-  // If no token is present, return 401 error
-  res.status(401).json({ message: "No token!" });
-}
+       const user = await User.findByUsername(decodedToken.username);
+
+       // Extracting admin key from user object if it exists
+       const adminKey = user?.adminKey?.toString() ?? "";
+       // Checking if extracted admin key matches with the one in env variables
+       if (adminKey !== key) {
+         // If admin key does not match, return 401 error
+         res.status(401).json({ message: "Must be an admin" });
+       } else {
+         // If admin key matches, let the endpoint continue executing
+         next();
+       }
+     }
+   });
+ } else {
+   // If no token is present, return 401 error
+   res.status(401).json({ message: "No token!" });
+ }
 };
