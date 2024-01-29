4133c02441d2ba7fe28b7d17ca71187271597b59
Jason <jasonbohio@gmail.com>
2024-01-29 14:33:48 -0500

    now

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -2,6 +2,10 @@ const knex = require("knex");
 
 const config = require("../knexfile");
 
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const env = process.env.NODE_ENV || "development";
 
 const db = knex(config[env]);
